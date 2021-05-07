ada2fe4b0dea001919b5c2e44c077eb8713759cb
Nelson Candanedo <74068040+uo720@users.noreply.github.com>
2021-05-07 23:55:07 +0200

    Simplified to have no h

--- NumberPartitioningDwave.py
+++ NumberPartitioningDwave.py
@@ -10,7 +10,7 @@ Q = {}
 J = {}
 h = {}
 values=[2,4,3,1]
-
+#values=[2,7,16,25,14,3,8,4,21,43,16,1]
 c=0
 for value in values:
   c+=value
@@ -18,26 +18,14 @@ i=0
 j=0
 while i < len(values):
     while j<len(values):
-        if i==j:
-            Q[(i,j)]=values[i]*(values[i]-c)
-            h[i]=values[i]*(values[i]-c)
-        else:
-            Q[(i,j)]=values[i]*values[j]
-            J[(i,j)]=values[i]*values[j]
+        J[(i,j)]=values[i]*values[j]
         j+=1
     i+=1
     j=0
 i=0
 j=0
 
-
-#We can create the model from the qubo matrix
-model=dimod.BinaryQuadraticModel.from_qubo(Q)
-print("The model we are going to solve in QUBO is")
-print(model)
-print()
-#Or directly using h and J
-model = dimod.BinaryQuadraticModel(h, J, 0.0, dimod.BINARY)
+model = dimod.BinaryQuadraticModel(h, J, 0.0, dimod.SPIN)
 print("The model we are going to solve in BinaryQuadratic is")
 print(model)
 print()
@@ -79,3 +67,4 @@ response = sampler.sample(model, num_reads=5000)
 print("The solution with D-Wave's quantum annealer called ",sampler_name,"en")
 print(response)
 print()
+
